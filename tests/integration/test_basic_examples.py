764a7495052aad32ec548be9078d0d5a93eee8cb
zunderwood <zev.utah@gmail.com>
2020-07-08 10:42:26 -0700

    FIX: Ry gate added with gate and integration test. New file added for gate integration tests called test_gates.py

--- tests/integration/test_basic_examples.py
+++ tests/integration/test_basic_examples.py
@@ -98,15 +98,5 @@ def test_multi_hadamard():
     assert max(result.counts.values()) - min(result.counts.values()) < 50
 
 
-def test_ry_int():
-    circuit = Circuit()
-    circuit.add(Qubits(1))
-    circuit.add(Ry(0,angle = np.pi/2))
-    circuit.add(Measure(0))
 
-    sess = QSession(backend=QuantumSimulator())
-    result = sess.run(circuit, num_shots=1000)
-
-    assert result['0']>450
-    assert result['1']>450
 
